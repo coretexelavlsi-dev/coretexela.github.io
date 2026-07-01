cc62846a6735b55fc40ca0cd799f02fec91034cc
CORETEXELA <coretexelavlsi@gmail.com>
2026-07-01 08:29:00 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 function startLearning() {
-  // direct flow to learning page (no login barrier)
   window.location.href = "learn.html";
 }
 
 function explorePlatform() {
-  // smooth scroll future upgrade or redirect
   window.location.href = "practice.html";
 }
